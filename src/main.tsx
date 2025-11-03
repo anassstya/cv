4895da584b92e88ce3547c3a3de75d98562a1d92
Anastasia <a.nasstya@mail.ru>
2025-11-03 21:46:02 +0300

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import './App.css';
+import { HashRouter } from "react-router-dom";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <Routes>
-                <Route path="/" element={<App />} />
-            </Routes>
-        </BrowserRouter>
+        <HashRouter>
+            <App />
+        </HashRouter>
     </React.StrictMode>
 );
